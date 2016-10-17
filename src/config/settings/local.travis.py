0b27d01539c08c364c90d037271a3945e3fe8378
Tom Valk <tomvalk@gmail.com>
2016-10-17 22:42:59 +0200

    [TASK] Add travis stub

---
+++ src/config/settings/local.travis.py
@@ -0,0 +1,21 @@
+
+# Database credentials and details
+MYSQL_DB = 'lurker'
+MYSQL_USERNAME = 'root'
+MYSQL_PASSWORD = ''
+MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
+
+# Generate unique secret key.
+SECRET_KEY = '7=999^z!d6ysczgfsdfa3got@u$5b$hhew=24!m27f_c$-+x7mm*'
+
+# Turn debug on or off
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# Activate if you want to secure the whole application with user accounts.
+GLOBAL_AUTHENTICATION = False
+
+# Enable if you are using a reverse proxy with HTTPS served to the client.
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SECURE_FORCE = True
---
+++ tools/pre-tests.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy local file for travis testing.
+cp ../src/config/settings/local.travis.py ../src/config/settings/local.py
