baae3f01f1fce0429ac9b90d173c1a1ecdd3ad86
Tom Valk <tomvalk@gmail.com>
2016-10-20 16:23:00 +0200

    [TASK] Add base for table view

--- src/apps/mails/views.py
+++ src/apps/mails/views.py
@@ -1,9 +1,26 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
 
+from apps.mails.models import Message
+
 
 class MailList(TemplateView):
 	template_name = 'mails/list.html'
 
 	def get(self, request, *args, **kwargs):
-		return render(request, self.template_name, {})
+
+		queryset = Message.objects.all()
+
+		table = {
+			'header': [
+				{
+					'name': 'peer',
+					'label': 'Peer',
+					'sort': True
+				}
+			]
+		}
+
+		return render(request, self.template_name, {
+			'table': table
+		})
