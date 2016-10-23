40e150f4dcbb0926a576f2019f942faf366d709d
Tom Valk <tomvalk@gmail.com>
2016-10-23 10:45:24 +0200

    [BUGFIX] Fix sidebar filter count for all messages

--- src/apps/core/context.py
+++ src/apps/core/context.py
@@ -8,28 +8,4 @@ def add_global_context(request):
 			'is_authenticated': request.user.is_authenticated(),
 			'is_superuser': getattr(request.user, 'is_superuser', False),
 		},
-
-		'total_mails': 51,
-		'filters': {
-			'servers': [
-				{
-					'id': 1,
-					'alias': 'DEV01',
-					'ip': '1.1.1.1',
-					'mails': 12
-				},
-				{
-					'id': 2,
-					'alias': 'DEV02',
-					'ip': '2.2.2.2',
-					'mails': 24
-				},
-				{
-					'id': 3,
-					'alias': 'DEV03',
-					'ip': '3.3.3.3',
-					'mails': 9
-				},
-			]
-		}
 	}
