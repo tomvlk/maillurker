22709369506eb9a63d7cdabc2166c89d657d67da
Tom Valk <tomvalk@gmail.com>
2016-10-30 10:37:29 +0100

    [TEST] Add more tests for filter models

--- src/test/filters/test_filter_storage.py
+++ src/test/filters/test_filter_storage.py
@@ -1,3 +1,5 @@
+from django.db import models as db_models
+
 from apps.accounts.models import User
 from ..base import TestCase
 
@@ -19,6 +21,14 @@ class FilterModelsTestCase(TestCase):
 		)
 		self.assertIsInstance(filterset, models.FilterSet)
 
+		self.assertEqual(filterset.name, 'Test Filter')
+		self.assertEqual(filterset.created_by_id, 1)
+		self.assertEqual(filterset.created_by.pk, 1)
+		self.assertEqual(filterset.is_global, True)
+		self.assertEqual(filterset.is_active, True)
+		self.assertEqual(filterset.icon, 'fa fa-filter')
+		self.assertEqual(filterset.combine, 'and')
+
 		rule = models.Rule.objects.create(
 			filter_set=filterset,
 			field='peer',
@@ -28,8 +38,18 @@ class FilterModelsTestCase(TestCase):
 		)
 		self.assertIsInstance(rule, models.Rule)
 
+		self.assertEqual(rule.filter_set, filterset)
+		self.assertEqual(rule.field, 'peer')
+		self.assertEqual(rule.operator, 'iexact')
+		self.assertEqual(rule.value, '127.0.0.1')
+		self.assertEqual(rule.negate, False)
+
 		filterset.delete()
 		rule.delete()
 
 		self.assertIsNone(filterset.pk)
 		self.assertIsNone(rule.pk)
+
+	def test_filter_meta(self):
+		self.assertEqual(models.FilterSet.get_combine_choices(), dict(models.FilterSet.COMBINE_CHOICES))
+		self.assertEqual(models.FilterSet.get_icon_choices(), dict(models.FilterSet.ICON_CHOICES))
