0a84d134a423da76edbef8f68b271bab2ef2e9ab
Tom Valk <tomvalk@gmail.com>
2016-10-30 19:13:08 +0100

    [TASK] Disable and update count of action sidebar.

--- src/apps/core/static/js/app.js
+++ src/apps/core/static/js/app.js
@@ -1,7 +1,21 @@
-$(function() {
-    /****************************
-     * Mail + Sidebar filtering *
-     ****************************/
+$(function () {
+  'use strict';
+  /****************************
+   * Mail + Sidebar filtering *
+   ****************************/
 
+  /**
+   * Sidebar - Action
+   */
+  $('.table-body-checkbox, .table-check-all').change(function() {
+    var number = $('.table-body-checkbox:checked').length;
+    $('#action-selection-count').text('' + number);
+
+    var disabled = true;
+    if (number > 0) {
+      disabled = false;
+    }
+    $('#action-buttons button').prop('disabled', disabled);
+  });
 
 });
