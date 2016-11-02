cec04b81577eb4053dc513432019fa2a2dec0ae6
Tom Valk <tomvalk@gmail.com>
2016-11-02 10:19:08 +0100

    [TASK] Add message detail modal.

--- src/apps/mails/static/js/mails.js
+++ src/apps/mails/static/js/mails.js
@@ -7,11 +7,95 @@ $(function () {
   $('.table-check-all').click(function () {
     var state = $(this).is(':checked');
 
-    $('.table-body-checkbox').each(function() {
+    $('.table-body-checkbox').each(function () {
       $(this).prop('checked', state);
     });
   });
 
-  alert(1);
+
+  /**
+   * Details View.
+   */
+  $('.table-body-detail-action').click(function () {
+    var template = $('#message-modal-template').html();
+    var listLinkTemplate = $('#message-modal-list-link-template').html();
+
+    var messageId = $(this).parents('tr').data('messageId');
+    var dialog = bootbox.dialog({
+      title: 'Message details',
+      size: 'large',
+      message: '<div class="text-center"><h6><i class="fa fa-spin fa-spinner"></i> Loading...</h6></div>'
+    });
+    dialog.init(function() {
+      var body = $(dialog).find('.bootbox-body');
+
+      var message;
+      var parts;
+      var part;
+
+      // Function called when all actions are completed.
+      function done () {
+        body.html(template);
+        $(dialog).find('.modal-title').text('Message from \'' + message.sender_name + '\'');
+
+        Object.keys(message).forEach(function (key) {
+          var elements = body.find('.field.'+key);
+          if (elements.length > 0) {
+            elements.text(message[key]);
+          }
+        });
+
+        // Custom formats
+        var addressFormatter = function (entry) {
+          return '' + entry[0] + ' <' + entry[1] + '>, ';
+        };
+        var headersText = '';
+        for (var headerKey in message.headers) {
+          if (message.headers.hasOwnProperty(headerKey))
+            headersText += headerKey + ': ' + message.headers[headerKey] + '\n';
+        }
+        body.find('.field.recipients_to_custom').text(message.recipients_to.map(addressFormatter));
+        body.find('.field.recipients_cc_custom').text(message.recipients_cc.map(addressFormatter));
+        body.find('.field.recipients_bcc_custom').text(message.recipients_bcc.map(addressFormatter));
+        body.find('.field.headers_custom').text(headersText);
+
+        // Links to parts and attachments
+        var partsList = body.find('div.parts');
+        var attachmentsList = body.find('div.attachments');
+
+        for (var idx in message.parts) {
+          if (! message.parts.hasOwnProperty(idx)) continue;
+          var part = message.parts[idx];
+          var listLink = $($.parseHTML('' + listLinkTemplate));
+
+          listLink.attr('href', '/mails/' + message.id + '/parts/' + part.id);
+
+          if (part.is_attachment) {
+            listLink.find('.icon').addClass('fa fa-paperclip');
+            listLink.find('.name').text('Attachment #'+idx+', '+part.type+'');
+            attachmentsList.append(listLink);
+          } else {
+            listLink.find('.icon').addClass('fa fa-envelope-o');
+            listLink.find('.name').text('Part #'+idx+', '+part.type+'');
+            partsList.append(listLink);
+          }
+        }
+
+        return {then: function() {}}
+      }
+
+      // Get message and first part.
+      $.get('/api/mails/' + messageId + '/').then(function (data) {
+        message = data;
+        parts = data.parts;
+
+        if (! parts) return done();
+        return $.get('/api/parts/' + parts[0].id + '/');
+      }).then(function (data) {
+        part = data;
+        return done();
+      });
+    });
+  });
 
 });
