5d18ca2c7673b7e3346bee49244355d6a81c1481
Tom Valk <tomvalk@gmail.com>
2016-11-03 10:06:30 +0100

    [TASK] Add real example files to lurker spoof command,.

--- src/apps/core/management/commands/spoof_lurker.py
+++ src/apps/core/management/commands/spoof_lurker.py
@@ -1,4 +1,9 @@
+import os
+import glob
+
 from random import Random
+
+from django.conf import settings
 from faker import Factory
 from django.core.management import BaseCommand
 from test.eml import send_eml
@@ -10,6 +15,9 @@ class Command(BaseCommand):
 	help = 'Spoof the lurker with some emails.'
 
 	def add_arguments(self, parser):
+		parser.add_argument('-t', '--type', nargs='*', type=str, choices=('random', 'examples'),
+							help='Type of messages.')
+
 		parser.add_argument('server', nargs='*', type=str, default='127.0.0.1',
 							help='Server to send to.')
 		parser.add_argument('port', nargs='*', type=int, default='1025',
@@ -19,15 +27,37 @@ class Command(BaseCommand):
 							help='Number of fake data to spoof.')
 
 	def handle(self, *args, **options):
+		message_type = options['type']
 		server = options['server']
 		port = options['port']
 		iterations = options['iterations']
 
-		for _ in range(0, iterations):
-			body_file = Random().randint(1, 6)
-			send_eml(
-				server=server, port=port,
-				path='random/{}.eml'.format(body_file),
-				from_addr=fake.company_email(),
-				to_addrs='{} <{}>'.format(fake.name(), fake.email()),
-			)
+		if 'random' in message_type:
+			for _ in range(0, iterations):
+				body_file = Random().randint(1, 6)
+				send_eml(
+					server=server, port=port,
+					path='random/{}.eml'.format(body_file),
+					from_addr=fake.company_email(),
+					to_addrs='{} <{}>'.format(fake.name(), fake.email()),
+				)
+
+		elif 'examples' in message_type:
+			mail_path = os.path.join(settings.TEST_DIR, 'files', 'eml')
+
+			for file in glob.iglob(os.path.join(mail_path, '*', '*.eml'), recursive=True):
+
+				to_addresses_count = Random().randint(1, 4)
+				to_addresses = list()
+				for _ in range(0, to_addresses_count):
+					to_addresses.append('{} <{}>'.format(fake.name(), fake.email()))
+
+				try:
+					send_eml(
+						server=server, port=port,
+						path=file,
+						from_addr=fake.company_email(),
+						to_addrs=', '.join(to_addresses)
+					)
+				except Exception as e:
+					print('Exception: ', e)
