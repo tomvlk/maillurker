9a75a7fcc76becbc7c2fb030b92fbdb6ffa4dda5
Tom Valk <tomvalk@gmail.com>
2016-11-04 15:30:22 +0100

    [TASK] Show personal filters in sidebar

--- src/apps/filters/context.py
+++ src/apps/filters/context.py
@@ -2,10 +2,18 @@ from apps.filters.models import FilterSet, Rule
 
 
 def add_global_context(request):
+
+	user_filters = list()
+
+	if request.user.is_authenticated():
+		user_filters = request.user.created_filters.filter(
+			is_global=False
+		)
+
 	return {
 		'apps_filters': {
 			'global_filters': FilterSet.objects.filter(is_active=True, is_global=True),
-			'user_filters': [],
+			'user_filters': user_filters,
 			'options': {
 				'icons': FilterSet.ICON_CHOICES,
 				'fields': Rule.FIELD_CHOICES,
