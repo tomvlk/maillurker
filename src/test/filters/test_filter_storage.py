5aa9a6c767d897e84e5dbaa7f7220f76c1f24b0a
Tom Valk <tomvalk@gmail.com>
2016-11-06 11:41:13 +0100

    [TEST] Change the way to import model in filter tests

--- src/test/filters/test_filter_storage.py
+++ src/test/filters/test_filter_storage.py
@@ -4,14 +4,14 @@ from apps.accounts.models import User
 from ..base import TestCase
 
 from apps.core.smtpd import Lurker
-from apps.filters import models
+from apps.filters.models import FilterSet, Rule
 
 
 class FilterModelsTestCase(TestCase):
 	fixtures = ['accounts/test_simple.json']
 
 	def test_filter_creation(self):
-		filterset = models.FilterSet.objects.create(
+		filterset = FilterSet.objects.create(
 			name='Test Filter',
 			created_by=User.objects.get(pk=1),
 			is_global=True,
@@ -19,7 +19,7 @@ class FilterModelsTestCase(TestCase):
 			icon='fa fa-filter',
 			combine='and'
 		)
-		self.assertIsInstance(filterset, models.FilterSet)
+		self.assertIsInstance(filterset, FilterSet)
 
 		self.assertEqual(filterset.name, 'Test Filter')
 		self.assertEqual(filterset.created_by_id, 1)
@@ -29,14 +29,14 @@ class FilterModelsTestCase(TestCase):
 		self.assertEqual(filterset.icon, 'fa fa-filter')
 		self.assertEqual(filterset.combine, 'and')
 
-		rule = models.Rule.objects.create(
+		rule = Rule.objects.create(
 			filter_set=filterset,
 			field='peer',
 			operator='iexact',
 			value='127.0.0.1',
 			negate=False
 		)
-		self.assertIsInstance(rule, models.Rule)
+		self.assertIsInstance(rule, Rule)
 
 		self.assertEqual(rule.filter_set, filterset)
 		self.assertEqual(rule.field, 'peer')
@@ -51,5 +51,7 @@ class FilterModelsTestCase(TestCase):
 		self.assertIsNone(rule.pk)
 
 	def test_filter_meta(self):
-		self.assertEqual(models.FilterSet.get_combine_choices(), dict(models.FilterSet.COMBINE_CHOICES))
-		self.assertEqual(models.FilterSet.get_icon_choices(), dict(models.FilterSet.ICON_CHOICES))
+		self.assertIsNotNone(FilterSet.ICON_CHOICES)
+		self.assertIsNotNone(FilterSet.COMBINE_CHOICES)
+		self.assertEqual(FilterSet.get_combine_choices(), dict(FilterSet.COMBINE_CHOICES))
+		self.assertEqual(FilterSet.get_icon_choices(), dict(FilterSet.ICON_CHOICES))
